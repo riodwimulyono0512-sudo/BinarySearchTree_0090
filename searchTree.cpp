2aabddb9a4d464a18f1884bf0ed796efd3d18f04
riodwimulyono0512_sudo <riodwimulyono0512@gmail.com>
2026-06-11 19:48:20 +0700

    membuat void inorder

--- searchTree.cpp
+++ searchTree.cpp
@@ -75,3 +75,11 @@ public:
                     currentNode = currentNode->rightchild;
          }
     } 
+
+      void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
