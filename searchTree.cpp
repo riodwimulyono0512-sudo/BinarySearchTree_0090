402e2a3fb98cfa369870d1bf827d7e806bea78cf
riodwimulyono0512_sudo <riodwimulyono0512@gmail.com>
2026-06-11 19:47:47 +0700

    kondisi jika element lebih kecil dari currentnode point to  info

--- searchTree.cpp
+++ searchTree.cpp
@@ -68,3 +68,10 @@ public:
              (currentNode->info != element))
              {
                 parent = currentNode;
+
+                if (element < currentNode->info)
+                    currentNode = currentNode->leftchild;
+                else
+                    currentNode = currentNode->rightchild;
+         }
+    } 
