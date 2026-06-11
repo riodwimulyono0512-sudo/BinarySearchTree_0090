5be5818d6ab41c3e52513f8ea8bba0095464159e
riodwimulyono0512_sudo <riodwimulyono0512@gmail.com>
2026-06-11 19:50:12 +0700

    membuat void postorder

--- searchTree.cpp
+++ searchTree.cpp
@@ -99,10 +99,27 @@ public:
             cout <<"Tree is empty" <<  endl;
             return;
         }
+
         if (ptr != NULL)
         {
             cout << ptr->info << " ";
             preorder(ptr->leftchild);
             preorder(ptr->rightchild);
+        }
+    }
+
+     void postorder(Node *ptr)
+    {
+        // perfoms the postorder traveersal of the tree
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
         }
     }
